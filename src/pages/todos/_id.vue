ea156a6fdd3b379a6eb5421d849ece9ec7c6b3da
ohjihoon2 <38777474+ohjihoon2@users.noreply.github.com>
2024-11-08 00:30:58 +0900

    todo 페이지 컴포넌트만들기

--- src/pages/todos/_id.vue
+++ src/pages/todos/_id.vue
@@ -3,35 +3,83 @@
   <div v-if="loading">
     Loading ...
   </div>
-  <form v-else>
-    <div class="form-group">
-      <label>Todo Subject</label>
-      <input v-model="todo.subject" type="text" class="form-control">
+  <form v-else @submit.prevent="onSave">
+    <div class="row">
+      <div class="col-6">
+        <div class="form-group">
+          <label>Todo Subject</label>
+          <input v-model="todo.subject" type="text" class="form-control">
+        </div>
+      </div>
+      <div class="col-6">
+        <div class="form-group">
+          <label>Status</label>
+          <div>
+            <button type="button" class="btn" :class="todo.completed ? 'btn-success' : 'btn-danger'" @click="toggleTodoStatus">
+                {{todo.completed ? 'Completed': 'Incomplete'}}
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-    <button class="btn btn-primary">save</button>
+    <button type="submit" class="btn btn-primary" :disabled="!todoUpdated">save</button>
+    <button class="btn btn-outline-dark ml-2" @click="moveToTodoListPage">cancel</button>
   </form>
 </template>
 
 <script>
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import axios from 'axios'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import _ from 'lodash'
 export default {
   setup () {
     const route = useRoute()
+    const router = useRouter()
+    const originalTodo = ref(null)
     const todo = ref(null)
     const loading = ref(true)
+    const todoId = route.params.id
 
     const getTodo = async () => {
-      const res = await axios.get('http://localhost:3000/todos/' + route.params.id)
-      todo.value = res.data
+      const res = await axios.get(`http://localhost:3000/todos/${todoId}`)
+      todo.value = { ...res.data }
+      originalTodo.value = { ...res.data }
       loading.value = false
+    }
+
+    const toggleTodoStatus = () => {
+      todo.value.completed = !todo.value.completed
+    }
+
+    const moveToTodoListPage = () => {
+      router.push({
+        name: 'Todos'
+      })
+    }
+
+    const todoUpdated = computed(() => {
+      return !_.isEqual(todo.value, originalTodo.value)
+    })
+
+    const onSave = async () => {
+      const res = await axios.put(`http://localhost:3000/todos/${todoId}`, {
+        subject: todo.value.subject,
+        completed: todo.value.completed
+      })
+
+      originalTodo.value = { ...res.data }
     }
     getTodo()
 
     return {
       todo,
-      loading
+      originalTodo,
+      loading,
+      toggleTodoStatus,
+      moveToTodoListPage,
+      onSave,
+      todoUpdated
     }
   }
 }
